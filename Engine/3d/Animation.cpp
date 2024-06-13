b79658293c67640fd8f828d6a2da9be6b6006db8
洋輝 大橋 <hiro0084169@outlook.jp>
2024-06-13 12:39:05 +0900

    直った＾＾

--- Engine/3d/Animation.cpp
+++ Engine/3d/Animation.cpp
@@ -15,7 +15,7 @@ void AnimationModel::Initialize(const std::string& filename, EulerTransform tran
 	AnimationModel::CreateDirectionalResource();
 
 	skeleton = CreateSkelton(modelData.rootNode);
-	skinCluster = CreateSkinCluster(dir_->GetDevice(), skeleton, modelData, dir_->GetSrvDescriptorHeap(), texture_->GetDiscreptorSize());
+	skinCluster = CreateSkinCluster(dir_->GetDevice(), skeleton, modelData, dir_->GetSrvDescriptorHeap2(), texture_->GetDiscreptorSize());
 
 	worldTransform_.Initialize();
 	worldTransform_.translate = transform.translate;
