cb5af07c195f88275c7fb9687f617bdb5bcfd575
洋輝 大橋 <hiro0084169@outlook.jp>
2024-07-22 10:37:10 +0900

    綺麗にした

--- Project/GameScene/TitleScene.cpp
+++ Project/GameScene/TitleScene.cpp
@@ -8,21 +8,8 @@ void TitleScene::Initialize()
 {
 	textureManager_->Initialize();
 
-	sprite_ = std::make_unique<Sprite>();
-	sprite_->Initialize(Vector2{ 0.0f, 0.0f }, Vector2{ 300.0f, 300.0f });
-
-	sprite2_ = std::make_unique<Sprite>();
-	sprite2_->Initialize(Vector2{ 300.0f, 300.0f }, Vector2{ 300.0f, 300.0f });
-
 	postProcess_ = std::make_unique<PostProcess>();
 	postProcess_->Initialize();
-
-	uv = textureManager_->Load("resources/uvChecker.png");
-	moon = textureManager_->Load("resources/moon.png");
-	monsterBall = textureManager_->Load("resources/monsterball.png");
-	kusa = textureManager_->Load("resources/kusa.png");
-	circle = textureManager_->Load("resources/circle.png");
-	cube = textureManager_->Load("resources/rostock_laage_airport_4k.dds");
 }
 
 
@@ -36,8 +23,6 @@ void TitleScene::Update()
 
 void TitleScene::Draw()
 {
-	sprite_->Draw(monsterBall);
-	sprite2_->Draw(cube);
 }
 
 void TitleScene::PostDraw()
